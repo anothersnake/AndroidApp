c6a0a8ecb4c5651edeb84c937e012dd0b6378278
ericnasanta <ericnasanta@gmail.com>
2017-05-14 17:57:05 -0600

    Added testActor

---
+++ Source/AndroidApp/AndroidApp.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AndroidApp.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AndroidApp, "AndroidApp" );
